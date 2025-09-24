0de6aafe1262ca59b4657790f5fd95eb7471c07b
Antonio <antoniorandria3342@gmail.com>
2025-09-24 16:22:00 +0300

    Initial commit

---
+++ src/main/java/FrontServlet.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class FrontServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+        // PrintWriter pw = arg1.getWriter();
+        // arg1.setContentType("text/html");
+        // pw.println("<a href=\"init\">Initialiser servlet</a><br>");
+        // pw.println("<a href=\"init-config\">Initialiser servlet dans config</a><br>");
+    }
+}
+
