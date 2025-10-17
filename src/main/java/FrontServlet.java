5317fd212509f2b483624debc55f274028a46972
Antonio <antoniorandria3342@gmail.com>
2025-10-17 14:37:56 +0300

    Update FrontServlet.java

--- src/main/java/FrontServlet.java
+++ src/main/java/FrontServlet.java
@@ -3,19 +3,23 @@ package com.example.front;
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
 
+@WebServlet("/")
 public class FrontServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        response.setContentType("text/html; charset=UTF-8");
-        
+
+        String path = request.getServletPath();
         String fullUrl = request.getRequestURL().toString();
         String queryString = request.getQueryString();
         if (queryString != null) {
             fullUrl += "?" + queryString;
         }
+
+        // Si fichier n existe pas
+        response.setContentType("text/html; charset=UTF-8");
 
         try (PrintWriter out = response.getWriter()) {
             out.println("<html><head><title>FrontServlet</title></head><body>");
