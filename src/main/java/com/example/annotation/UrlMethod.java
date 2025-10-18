e0b838e0a08dfa3eca0b956843ae8f6c77398b77
Antonio <antoniorandria3342@gmail.com>
2025-10-18 22:26:43 +0300

    Delete UrlMethod.java

--- src/main/java/com/example/annotation/UrlMethod.java
+++
@@ -1,8 +0,0 @@
-package com.example.annotation;
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface UrlMethod {
-    String path() default "";
-}
